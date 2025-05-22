584f67d11c4302a966bc0b59bffa3d5209753eff
abyss-s <lh3729@naver.com>
2025-05-22 11:46:26 +0900

    feat: axios 인스턴스 초기설정

---
+++ src/lib/axiosInstance.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+import { ENV } from "./env";
+
+export const axiosInstance = axios.create({
+  baseURL: ENV.API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
---
+++ src/lib/env.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+  API_BASE_URL: `${process.env.VITE_API_BASE_URL}`,
+  // KAKAO_JAVASCRIPT_KEY: `${process.env.VITE_KAKAO_JAVASCRIPT_KEY}`,
+} as const;
