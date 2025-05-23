099109c7f8d839c6a5a7b45cef46b4c3858c3d36
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2025-05-24 01:49:28 +0900

    FEAT: 구글 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/reset.css";
 import Layout from "@/components/Layout/Layout";
 import QueryProvider from "./_components/QueryProvider";
 import MockInterceptorProvider from "./_components/MockInterceptorProvider";
+import AppHeader from "@/components/AppHeader";
 
 export const metadata: Metadata = {
   title: "My App",
@@ -13,10 +14,16 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=League+Gothic&display=swap" rel="stylesheet" />
+      </head>
       <body className="body">
         <QueryProvider>
           <Layout>
             <MockInterceptorProvider />
+            <AppHeader />
             {children}
           </Layout>
         </QueryProvider>
